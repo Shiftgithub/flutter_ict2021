532fc202e542d2dcd61fe5ebaf46daba8616b82a
FlutLab.io <>
2021-11-16 13:30:41 +0000

    Autosave

--- lib/newpage.dart
+++ lib/newpage.dart
@@ -3,14 +3,32 @@ import 'package:flutter/material.dart';
 class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SecondHomePage(),
+    );
+  }
+}
+
+class SecondHomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<SecondHomePage> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Mamun'),
-          color: Colors.blue,
+      appBar: AppBar(
+        title: Text('Happy Birthday Mumu'),
+        centerTitle: true,
+        foregroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Image(
+            image: AssetImage('images/mumu10.jpg'),
+          ),
         ),
       ),
     );
