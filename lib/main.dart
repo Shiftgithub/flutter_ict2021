c24bf4c72500afd7f45a4daece326db029ad5572
FlutLab.io <>
2021-11-16 13:31:50 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _HomePageState extends State<HomePage> {
                 );
               },
               child: Text('Next'),
-              color: Colors.red,
+              color: Colors.black,
             ),
           ],
         ),
